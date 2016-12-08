dfaf8c4bcb8013be4e8d219bb933f0ff823138ff
Fredrik Wiklander <freny370@gmail.com>
2016-12-08 10:53:30 +0100

    Removed unused python code

--- src/TeamCityRequests.py
+++ src/TeamCityRequests.py
@@ -3,15 +3,6 @@ import requests
 base_uri = ''
 
 
-def get_all_builds(url):
-    request_headers = get_default_request_headers()
-    url = url.replace(' ', '')
-    response = requests.get(url, headers=request_headers)
-
-    all_builds = response.json()
-    return all_builds['build']
-
-
 def start_build(config_id, dependency_build_ids):
     request_headers = get_default_request_headers()
     uri = base_uri + '/app/rest/buildQueue'
@@ -36,19 +27,6 @@ def start_build(config_id, dependency_build_ids):
     # print ('Starting build for ' + config_id + ' build no. ' + dependency_build_id)
     response = requests.post(uri, json=payload)
     return response
-
-
-def get_build(uri, build_number):
-    request_headers = get_default_request_headers()
-
-    if not uri.endswith('/'):
-        uri += '/'
-
-    uri += 'id: ' + str(build_number)
-    uri = uri.replace(" ", "")
-
-    response = requests.get(base_uri + uri, headers=request_headers)
-    return dict(status_code=response.status_code, payload_json=response.json())
 
 
 def make_request(uri):
